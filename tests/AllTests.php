081714c34fe53adcbf4b78bd4ce237b9722a4968
Daniel O'Connor <clockwerx@php.net>
2010-02-12 04:56:11 +0000

    Fixed PHPUnit 3.4 compatibility
    git-svn-id: http://svn.php.net/repository/pear/packages/Net_UserAgent_Mobile/trunk@294986 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -41,7 +41,6 @@ if (!defined('PHPUnit_MAIN_METHOD')) {
     define('PHPUnit_MAIN_METHOD', 'Net_UserAgent_Mobile_AllTests::main');
 }
 
-require_once 'PHPUnit/Framework/TestSuite.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
 
 chdir(dirname(__FILE__) . '/..');
